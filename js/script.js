c664714c2b5bd772b18a9e103f9404c70d38d8ac
JuanFerrer <juanemilioferrer@outlook.com>
2017-05-11 16:51:14 +0100

    Starting own library
    Removed jdenticon
    Implemented a version of identicon from
    https://github.com/stewartlord/identicon.js

--- js/script.js
+++ js/script.js
@@ -3,4 +3,5 @@ var str = Math.random().toString(36).substring(7);
 shaObj.update(str);
 var h = shaObj.getHash("HEX");
 
-document.getElementById("identicon").setAttribute("data-jdenticon-hash", h);
+var data = new Identicon(h, 420).toString();
+document.getElementById("identicon").setAttribute("src", 'data:image/png;base64,' + data);
