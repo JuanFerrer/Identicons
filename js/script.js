45ce274d82f85170d37ae296975ad4fc625ce3a0
JuanFerrer <juanemilioferrer@outlook.com>
2017-05-12 10:46:28 +0100

    Create identicon from input
    Consistently repeats identicon for input

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
-var shaObj = new jsSHA("SHA-512", "TEXT");
-var str = Math.random().toString(36).substring(7);
-//str = "v6sqiv"; //#
-shaObj.update(str);
-var h = shaObj.getHash("HEX");
+document.getElementById("input-bar").addEventListener("input", updateIdenticon)
 
-var data = new Identicon(h, 420).toString();
-document.getElementById("identicon").setAttribute("src", 'data:image/png;base64,' + data);
+
+updateIdenticon();
+
+function updateIdenticon() {
+    var shaObj = new jsSHA("SHA-512", "TEXT");
+	//var str = Math.random().toString(36).substring(7);
+	var str = document.getElementById("input-bar").value;
+	console.log(str);
+	//str = "v6sqiv"; //#
+	shaObj.update(str);
+	var h = shaObj.getHash("HEX");
+
+	var data = new Identicon(h, 420).toString();
+	document.getElementById("identicon").setAttribute("src", 'data:image/png;base64,' + data);
+}
